6e95db2c8e466003eb36c3dd3f8cae5f38c94f2d
Rui Alves <up201606746@fe.up.pt>
2019-12-04 08:07:14 +0000

    Add day 4 part 2

---
+++ Day 4/part2.js
@@ -0,0 +1,43 @@
+const { readFile } = require("../utils/reader");
+
+const [from, to] = readFile("input.txt")[0].split("-").map((num) => parseInt(num, 10));
+
+const getDigits = (num) => {
+    let number = num;
+    const digits = [];
+    for (let i = 0; i < 6; ++i) {
+        digits.unshift(number % 10);
+        number = Math.floor(number / 10);
+    }
+    return digits;
+};
+
+const rule1 = (digits) => {
+    const aux = ["_"].concat(digits).concat(["_"]);
+    for (let i = 1; i < 6; ++i) {
+        if (aux[i] === aux[i + 1] && aux[i] !== aux[i - 1] && aux[i + 1] !== aux[i + 2]) {
+            return true;
+        }
+    }
+    return false;
+};
+
+const rule2 = (digits) => {
+    for (let i = 0; i < 5; ++i) {
+        if (digits[i] > digits[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+};
+
+const isPassword = (num) => {
+    const digits = getDigits(num);
+    return rule1(digits) && rule2(digits);
+};
+
+let count = 0;
+for (let i = from; i <= to; ++i) {
+    if (isPassword(i)) ++count;
+}
+console.log(count);
