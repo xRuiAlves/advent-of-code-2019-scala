e36e0317436bab77ecfd0a95a07cd85e5daa339a
Rui Alves <up201606746@fe.up.pt>
2020-10-17 13:17:45 +0100

    Remove Javascript, switching to Scala

--- Day 1/part1.js
+++
@@ -1,7 +0,0 @@
-const { readFile } = require("../utils/reader");
-
-const getFuel = (mass) => Math.floor(mass / 3) - 2;
-
-const data = readFile("input.txt");
-const ans = data.map(getFuel).reduce((prev, curr) => prev + curr);
-console.log(ans);
--- utils/reader.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-const readFile = (file_name) => (
-    fs.readFileSync(file_name).toString().split("\n")
-);
-
-module.exports = {
-    readFile,
-};
